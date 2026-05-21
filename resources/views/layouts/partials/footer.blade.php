b9cb9649c2e5f72882419f044cfafbf200fb06e3
Ramiro-Davila <150105216+Ramiro-Davila@users.noreply.github.com>
2026-05-21 10:25:25 -0600

    Create footer.blade.php

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-neutral-primary border-t border-default mt-auto">
+    <div class="max-w-screen-xl mx-auto py-8 px-4">
+        <div class="text-center text-body text-sm">
+            <p>&copy; {{ date('Y') }} Mi Aplicación Laravel. Todos los derechos reservados.</p>
+        </div>
+    </div>
+</footer>
