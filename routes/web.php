440db8f50f816825d672dd09f100ac72297c2b4d
Ramiro-Davila <150105216+Ramiro-Davila@users.noreply.github.com>
2026-05-21 10:30:22 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecetaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,5 +20,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/receta/create', function() {
+    return view('recetas.create');
+})->name('recetas.create');
+
+Route::get('/receta/{receta}', [RecetaController::class, 'show'])->name('recetas.show');
+Route::post('receta/store', [RecetaController::class, 'store'])->name('recetas.store');
+Route::get('/receta', [RecetaController::class, 'index'])->name('recetas');
 
 require __DIR__.'/auth.php';
