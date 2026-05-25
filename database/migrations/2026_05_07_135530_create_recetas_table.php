77376a40c0daac3f5514fe3569bd2ff37060ec4b
Ramiro-Davila <150105216+Ramiro-Davila@users.noreply.github.com>
2026-05-25 11:16:52 -0600

    Update 2026_05_07_135530_create_recetas_table.php

--- database/migrations/2026_05_07_135530_create_recetas_table.php
+++ database/migrations/2026_05_07_135530_create_recetas_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('recetas', function (Blueprint $table) {
             $table->id('id_receta');
             $table->string('nombre_receta', 150);
+            $table->string('slug')->unique();
+            $table->longText('descripcion')->nullable();
+            $table->text('procedimiento')->nullable();
+            $table->string('imagen')->nullable();
             $table->integer('porciones')->nullable();
-            $table->foreignId('id_usuario')->constrained('users', 'id_usuario')->onDelete('cascade');
+            $table->foreignId('id_usuario')->nullable()->constrained('users', 'id_usuario')->onDelete('cascade');
             $table->date('fecha_creacion');
             $table->decimal('costo_total', 10, 2)->nullable();
             $table->decimal('precio_sugerido', 10, 2)->nullable();
