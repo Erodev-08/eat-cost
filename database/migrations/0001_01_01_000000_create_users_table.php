9b0ff1c07d64a3d6306d2c1bf41bf674b62c932f
Ramiro-Davila <150105216+Ramiro-Davila@users.noreply.github.com>
2026-05-25 11:16:14 -0600

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,14 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('id_usuario');
+            $table->string('nombre', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('contrasena', 255);
             $table->string('institution')->nullable();
+            $table->enum('rol', ['estudiante', 'admin'])->default('estudiante');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); // created_at, updated_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
